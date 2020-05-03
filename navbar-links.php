a483a4e851243c962c39ca457ec20cf1d7479fc3
n.ayesh <n.ayesh@newline.ps>
2020-05-03 16:20:11 +0300

    first commit

---
+++ navbar-links.php
@@ -0,0 +1,15 @@
+<li class="nav-item active">
+    <a class="nav-link" href="#">Overview <span class="sr-only">(current)</span></a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="#">During Program</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="#">Criteria</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="#">Timeline</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="#">Schedule</a>
+</li>
